599c726093d56d8821a9623be5dc1c31687ff106
sunero4 <anderskloborg@gmail.com>
2023-12-03 21:17:51 +0000

    Solved day 2 part 2 in scala.

--- aoc-scala/Day2.scala
+++ aoc-scala/Day2.scala
@@ -1,11 +1,16 @@
 import java.util.Stack
 import scala.collection.mutable.HashMap
 
-@main def Day2: Unit =
-    val colors = HashMap[String, Int]("red"->12, "green"->13, "blue"->14)
 
+@main def Day2: Unit =
+    part1()
+    part2()
+
+def part1() : Unit =
     val fileName = "../inputs/day2.txt"
     val bufferedSource = scala.io.Source.fromFile(fileName)
+
+    val colors = HashMap[String, Int]("red"->12, "green"->13, "blue"->14)
 
     val sumOfValidGameIds = bufferedSource.getLines()
         .map(l => getGameIdAndRest(l))
@@ -13,10 +18,23 @@ import scala.collection.mutable.HashMap
         .map((gameId, rest) => gameId)
         .sum
 
-    println(sumOfValidGameIds)
+    println(s"Answer to part 1: $sumOfValidGameIds")
 
     bufferedSource.close()
 
+def part2() : Unit =
+    val fileName = "../inputs/day2.txt"
+    val bufferedSource = scala.io.Source.fromFile(fileName)
+
+    val sumOfProducts = bufferedSource.getLines()
+        .map(l => getGameIdAndRest(l))
+        .map((gameId, rest) => getMinimumAmountOfEachColor(gameId, rest))
+        .map(counts => counts.product)
+        .sum
+
+    println(s"Answer to part 2: $sumOfProducts")
+
+    bufferedSource.close()
 
 def getGameIdAndRest(line: String) : (Int, String) =
     val Array(idPart, rest) = line.split(":", 2).map(x => x)
@@ -53,4 +71,26 @@ def isValidGame(gameId: Int, rest: String, colors: HashMap[String,Int]): Boolean
         })).flatMap(x => x).reduce((a,b) => a && b)
 
 
-    
+def getMinimumAmountOfEachColor(gameId: Int, rest: String): Array[Int] =
+    val sections = rest.split(';')
+
+    // index 0 is red, 1 is green, 2 is blue
+    val maxAmounts = Array(0, 0, 0)
+
+    sections.flatMap(section => section.split(',').map(s => {
+        val Array(n, color) = s.trim().split(" ", 2)
+        val amount = parseNum(n)
+
+        (color, amount)
+    })).foreach((color, amount) => {
+        val index = color match
+            case "red" => 0
+            case "green" => 1
+            case _ => 2
+
+        if amount > maxAmounts(index) then {
+            maxAmounts.update(index, amount)
+        }
+    })
+
+    maxAmounts
