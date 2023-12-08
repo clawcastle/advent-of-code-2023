da01a78f3415f183861f84d06709dd5ebf82d1ff
anderskloborg <anderskloborg@gmail.com>
2023-12-08 16:06:14 +0000

    solved day 4 part 1 in scala.

--- aoc-scala/Day4.scala
+++ aoc-scala/Day4.scala
@@ -3,12 +3,22 @@
     val bufferedSource = scala.io.Source.fromFile(fileName)
 
     val x = bufferedSource.getLines()
-        .map(line => line.split(":")(1).split('|').map(parts => parts.map(n => n.toInt)))
-        .map(nums => {
-            val winning = nums(0)
-            val cardNumbers = nums(1)
-            
-            
-        })
+        .map(line => line.split(":")(1).split('|').map(parts => parts.split(' ').filter(p => p != "").map(n => n.toInt)))
+        .map(nums => getCardScore(nums(0), nums(1)))
+        .sum
+
+
+    println(x)
 
     bufferedSource.close()
+
+    
+def getCardScore(winningNumbers: Array[Int], cardNumbers: Array[Int]): Int =    
+    var score = 1
+
+    val winningCardNumbers = cardNumbers.count(n => winningNumbers.contains(n))
+
+    score = score << winningCardNumbers
+    score = score >> 1
+
+    return score
