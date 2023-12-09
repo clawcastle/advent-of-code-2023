3ba73d124e6bd4bc9d5086b58853b07ff56510ae
anderskloborg <anderskloborg@gmail.com>
2023-12-09 14:19:48 +0000

    solved day 4 part 2 in scala.

--- aoc-scala/Day4.scala
+++ aoc-scala/Day4.scala
@@ -1,5 +1,8 @@
+import java.util.Stack
+
 @main def Day4: Unit =
     part1()
+    part2()
 
 class Card(val winningNumbers: Array[Int], val cardNumbers: Array[Int])
 
@@ -22,9 +25,42 @@ def part2(): Unit =
 
     val lines = bufferedSource.getLines().toArray
 
-    for (line <- lines) {
+    val indicesOfLinesToProcess = Stack[Int]()
+    var currentIndex = 0
 
+    var count = 0
+
+    val cards: Array[Option[Card]] = Array.fill(lines.length)(None)
+
+    while currentIndex < lines.length do {
+        indicesOfLinesToProcess.push(currentIndex)
+
+         while !indicesOfLinesToProcess.empty() do {
+            val i = indicesOfLinesToProcess.pop()
+            count = count + 1
+
+            if i < lines.length then {
+                if cards(i) == None then {
+                    cards.update(i, Some(parseCardLine(lines(i))))
+                }
+                val card = cards(i).get
+                
+                val winningCardNumbers = card.cardNumbers.count(n => card.winningNumbers.contains(n))
+
+                val newIndicesToProcess = Range.inclusive(i + 1, i + winningCardNumbers)
+
+                newIndicesToProcess.foreach(n => {
+                    if n < lines.length then {
+                        indicesOfLinesToProcess.push(n)
+                    }
+                })
+            }
+        }
+
+        currentIndex = currentIndex + 1
     }
+
+    println(s"Part 2 result: $count")
 
     bufferedSource.close()
 
