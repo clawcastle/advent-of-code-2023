c11a5818caa64877323ef12fabdb5d59acc05391
sunero4 <anderskloborg@gmail.com>
2023-12-10 10:23:16 +0000

    Solved day 1 part 1 scala

--- aoc-scala/Day1.scala
+++ aoc-scala/Day1.scala
@@ -1,5 +1,8 @@
+import scala.collection.mutable.HashMap
+
 @main def Day1 : Unit =
     part1()
+    part2()
 
 def part1() : Unit =
     val fileName = "../inputs/day1.txt"
@@ -19,3 +22,59 @@ def part1() : Unit =
     println(sumOfNumbers)
 
     bufferedSource.close()
+
+
+def part2() : Unit =
+    val fileName = "../inputs/day1.txt"
+    val bufferedSource = scala.io.Source.fromFile(fileName)
+
+    val windowSize = 3 // Longest number string
+
+    val x = bufferedSource.getLines().map(line => {
+        val firstDigit = line
+            .sliding(windowSize)
+            .find(window => getDigitFromWindow(window) != None)
+            .flatMap(window => getDigitFromWindow(window))
+            .getOrElse(0)
+
+        val lastDigit = line
+            .sliding(windowSize)
+            .filter(window => getDigitFromWindow(window) != None)
+            .
+
+        println(s"first: $firstDigit, last: $lastDigit")
+
+        (firstDigit * 10) + lastDigit
+    }).sum
+
+    println(x)
+
+    bufferedSource.close()
+
+
+val numbers = HashMap[String, Int](
+    "zero"->0, 
+    "one"->1, 
+    "two"->2, 
+    "three"->3, 
+    "four"->4, 
+    "five"->5, 
+    "six"->6, 
+    "seven"->7, 
+    "eight"->8, 
+    "nine"->9
+    )
+
+def getDigitFromWindow(s: String) : Option[Int] =
+    if s(0) >= '0' && s(0) <= '9' then {
+        return Some(s.charAt(0).toInt - '0')
+    }
+
+    for (i <- Range.inclusive(0, 4)) {
+        val key = s.slice(0, i + 1)
+        if numbers.contains(key) then return Some(numbers(key))
+    }
+
+    println(s)
+
+    return None
